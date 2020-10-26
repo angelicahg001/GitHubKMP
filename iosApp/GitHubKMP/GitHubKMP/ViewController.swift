660482e13160e8bc8b297465362730a886490b90
AngelicHG <angelicahg001@gmail.com>
2020-10-26 00:47:57 -0600

    create and config ios project

---
+++ iosApp/GitHubKMP/GitHubKMP/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GitHubKMP
+//
+//  Created by Jose Luis Cadena on 25/10/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
